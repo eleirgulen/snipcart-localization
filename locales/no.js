346cd82f6573db604582f4c1856bb3bb3885f5c1
eleirgulen <eirik@flerekunder.no>
2014-05-02 12:29:17 +0200

    Update no.js

--- locales/no.js
+++ locales/no.js
@@ -1,1 +1,292 @@
-d
+Snipcart.execute('registerLocale', 'no', {
+yes:
+"Ja",
+no:
+"Nei",
+print:
+"Utskrift",
+download_as_pdf:
+"Last ned som PDF",
+checkout:
+"Gå til betaling",
+close:
+"Lukk",
+name:
+"Navb",
+company_name:
+"Firmanavn",
+share_by_email:
+"Del med epost",
+email:
+"Epost",
+password:
+"Passord",
+confirm_password:
+"Bekreft passord",
+ok:
+"OK",
+send:
+"Send",
+address_1:
+"Gateadresse",
+address_2:
+"Gateadresse 2",
+city:
+"Sted",
+postal_code:
+"Postnummer",
+phone:
+"Telefon",
+previous:
+"Forrige steg",
+next:
+"Neste steg",
+finalize:
+"Bekreft ordre",
+country:
+"Land",
+subtotal:
+"Subtotal",
+rebate:
+"Rabatt",
+apply_promo_code:
+"Rabattkode",
+my_cart:
+"Min kurv",
+continue_shopping:
+"Fortsett å handle",
+my_cart_content:
+"Min handlekurv",
+shipping_method:
+"Fraktalternativer",
+payment_method:
+"Betalingsmåte",
+confirm_order:
+"Bekreft ordre",
+bill_me_later:
+"Fakturer senere",
+bill_me_later_explanation:
+"En faktura blir sendt per epost.",
+promo_code_applied_successfully:
+"Din rabatt har blitt trekt fra ordren",
+promo_code_is_invalid:
+"Rabattkoden er feil.",
+promo_code_code:
+"Har du rabattkode?",
+promo_code_rate_on_order:
+"rabatt på ordren",
+promo_code_alternate_price:
+"spesialpris på produkter",
+total:
+"Total",
+province_state:
+"Fylke",
+billing_address:
+"Fakturadresse",
+shipping_address:
+"Leveringsadresse",
+payment_informations:
+"Betalingsinforamsjon",
+payment_informations_bill_me_later:
+"Jeg vil betale senere",
+payment_informations_paypalexpress:
+"Betal med Paypal",
+credit_card_type_mastercard:
+"Mastercard",
+credit_card_type_visa:
+"Visa",
+credit_card_type_amex:
+"American Express",
+months_january:
+"januar",
+months_february:
+"februar",
+months_march:
+"mars",
+months_april:
+"april",
+months_may:
+"februar",
+months_june:
+"juni",
+months_july:
+"juli",
+months_august:
+"august",
+months_september:
+"september",
+months_october:
+"oktober",
+months_november:
+"november",
+months_december:
+"desember",
+cart_items_table_item:
+"Produkt",
+cart_items_table_description:
+"Beskrivelse",
+cart_items_table_quantity:
+"Antall",
+cart_items_table_unit_price:
+"Pris pr. stykk",
+cart_items_table_total_price:
+"Totalpris",
+cart_empty_text:
+"Handlekurven er tom, velg produkt før du går til betaling",
+new_account_form_create_new_account:
+"Lag en konto",
+new_account_form_create_new_account_action:
+"Lag en konto",
+login_form_having_an_account:
+"Logg inn",
+login_form_login_action:
+"Log inn",
+login_form_forgot_password_action:
+"Jeg har glemt mitt passord",
+forgot_password_forgot_your_password:
+"Glemt passord?",
+forgot_password_please_enter_email:
+"Skriv inn din epost og vi vil sende deg en link for å lage ett nytt passord.",
+forgot_password_success_email_sent:
+"Epost er sendt",
+forgot_password_email_sent_message:
+"En epost har blitt sendt med instruksjoner om hvordan du kan oppretter ett nytt passord.",
+login_checkout_as_guest:
+"Betale uten å opprette konto",
+login_checkout_as_guest_notice:
+"Når betaling er fullført får du mulighet til å opprette en konto hos oss for å se ordrestatus med mer.",
+shipping_address_same_as_billing:
+"Bruk denne adressen for levering",
+shipping_method_method_name:
+"Leveringsmåte",
+shipping_method_shipping_price:
+"Fraktpris",
+shipping_method_failure_message:
+"Vi har ikke klart å finne ett fraktalternativ. Vennligst prøv en annen adresse eller endre fraktmetode.",
+shipping_method_failure_click_here_to_edit:
+"Trykk her for å endre leveringsmetode",
+payment_method_card_holder:
+"Navn på kort",
+payment_method_card_type:
+"Korttype",
+payment_method_card_number:
+"Kortnummer",
+payment_method_card_cvc:
+"CSC",
+payment_method_card_exp_month:
+"Utløp måned / år",
+payment_method_card_exp_year:
+"Utløpsår",
+payment_method_cvc_infos:
+"CVC koden er de 3 tall med sikkerhetsnummer på baksiden av kreditkortet, normalt til høyre for din signatur.",
+create_an_account:
+"Opprett en konto",
+why_create_account:
+"For raksere betaling neste gang så anbefaler vi å opprette en konto.",
+reset_password:
+"Nullstill passord",
+reset_password_success:
+"Passord er nullstillet.",
+reset_password_changed:
+"Ditt passord er endret.",
+reset_password_click_here_to_login:
+"Trykk her for å logge inn",
+thankyou_message:
+"Takk for din bestilling! Din kvittering er sendt til deg på epost",
+thankyou_submessage:
+"Du vil snart motta en bekreftelse på epost",
+account_created_successfully:
+"Konto opprettet",
+account_created_successfully_message:
+"Din konto er opprettet,tusen takk.",
+errors_required:
+"Dette feltet er obligatorisk",
+errors_email_must_be_unique:
+"En bruker med samme epost ekstistere allerede",
+errors_both_password_must_match:
+"Begge passordene må være like",
+errors_email_must_be_valid:
+"Din epost må være en gyldig epostadresse",
+errors_email_does_not_match_any_existing_user:
+"Ingen bruker med denne epost eksisterer",
+errors_email_does_not_match_reset_password_request:
+"Din epost passer ikke til noe som har forespurt å nullstille passordet.",
+errors_reset_password_token_expired:
+"Kode for å nullstille passordet har utløpt.",
+errors_invalid_authentication_infos:
+"Ugyldig innformasjon for authentikasjon",
+error_payment_items_empty:
+"Din ordre er ikke gyldig. Vennligst last siden på nytt. Ditt kredittkort er ikke belastet.",
+error_payment_items_are_invalid:
+"We have not been able to complete your order. It seems that one of the items in your cart has an invalid price.",
+error_crawling_failed:
+"We have not been able to validate your order, your credit card has not been charged, please try again in a few moments.",
+powered_by:
+"Powered and secured by",
+promocode_rate_format:
+"{0}% discount on your order",
+promocode_amount_format:
+"{0} discount on your order",
+shipping_method_business_days:
+"{0} business days",
+shipping_method_business_day:
+"{0} business day",
+shipping_method_delivery_time:
+"By {0}", // By 2013-11-28
+welcome:
+"Welcome",
+back:
+"Back",
+order_infos:
+"Order infos",
+generic_error_title:
+"Oops, an error occured.",
+promocode_deleted_at_checkout:
+"The Discount you used has reached its usage limit while you were doing your checkout. Sorry for this inconvenience.",
+continue_shopping:
+"Continue shopping",
+payment_required_message:
+"The shopping cart for this website has been disabled. If you are the owner of this website, please login into Snipcart dashboard to resolve the issue.",
+payment_require_title:
+"The shopping cart is disabled.",
+configuration_problem:
+"Configuration problem",
+additionnal_information:
+"Enter a message below if you want to send feedback or more information about this problem.",
+send_error:
+"Send this error to website's owner",
+message_sent:
+"Message sent, thank you",
+paypalexpress_loading:
+"You will be redirected to Paypal to make the payment soon.",
+paypalexpress_cancelled:
+"You cancelled the transaction, you can either click on the button below to try again or simply continue to shop.",
+retry:
+"Try again",
+error_crawlingfailed_title:
+"Something went wrong when validating your order, don't worry, you have not been charged.",
+error_crawling_unreachable:
+"Item <strong>{0}</strong> is unreachable at <strong>{1}</strong>. Make sure the product URL is publicly available.",
+error_crawling_product_not_found:
+"Item <strong>{0}</strong> has not been found at <strong>{1}</strong>.",
+error_crawling_price_not_found:
+"Item <strong>{0}</strong> does not have any specified price at <strong>{1}</strong>, specify it with data-item-price.",
+error_crawling_price_doesnot_match:
+"Item <strong>{0}</strong> price at <strong>{3}</strong> is <strong>{2}</strong> but should be <strong>{1}</strong>.",
+error_crawlingfailed_title_test:
+"Noe gikk galt ved belasting. Ikke bekymre deg, da ordren ikke er belastet ditt kort. Denne nettsiden er i testmodus..",
+order_completedon:
+"Ordredato",
+payment_method_status:
+"Transaksjonstatus",
+payment_method_status_approved:
+"Godkjent",
+order_reference_number:
+"Referansenummer",
+order_transaction_amount:
+"Transaksjonsbeløp",
+order_invoice_number:
+"Fakturanummer",
+order_authorization_code:
+"Autoriasasjonskode"
+});
